9a9de76fe81c4cbd1d51ea0a286e18b50b2d63b4
Klaus Schneider <klaus@email.arizona.edu>
2015-09-26 14:58:40 -0700

    Added documentation

--- daemon/table/cs-decision-policy.hpp
+++ daemon/table/cs-decision-policy.hpp
@@ -6,15 +6,26 @@
 namespace nfd {
 namespace cs{
 
+/**
+ * \brief Represents the CS cache decision policy.
+ *
+ */
 class DecisionPolicy
 {
 public:
   DecisionPolicy() {};
+
+
+  /**
+   * \brief Decides if to cache an incoming data packet.
+   *
+   * Returns true if the packet should be cached, false otherwise.
+   */
   virtual bool admitPacket(const Data & data) = 0;
 };
 
-
 } // namespace cs
 } // namespace nfd
+
 
 #endif
